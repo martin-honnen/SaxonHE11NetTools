5a9e59f080de03a7d9c0d071591e0fc487aa90ef
Martin Honnen <martin.honnen@gmx.de>
2022-09-14 13:49:45 +0200

    XQuery 3.1 command line runner with Invisible XML support through CoffeeSacks extension library

---
+++ SaxonHE11NetIXQuery/InvisibleXmlXQuery.cs
@@ -0,0 +1,37 @@
+﻿using net.sf.saxon;
+using org.nineml.coffeesacks;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace SaxonHE11NetIXQuery
+{
+    public class InvisibleXmlXQuery : Query
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"SaxonHENetIXQuery 11.4-alpha1 on .NET {Environment.Version} {Environment.OSVersion}");
+
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            ikvm.runtime.Startup.addBootClassPathAssembly(Assembly.Load("org.xmlresolver.xmlresolver"));
+            ikvm.runtime.Startup.addBootClassPathAssembly(Assembly.Load("org.xmlresolver.xmlresolver_data"));
+
+            var invisbleXmlXQuery = new InvisibleXmlXQuery();
+
+            invisbleXmlXQuery.doQuery(args, "IXQuery");
+
+            stopWatch.Stop();
+
+            Console.WriteLine($"Elapsed time: {stopWatch.Elapsed}");
+
+        }
+
+        protected override void initializeConfiguration(Configuration config)
+        {
+            var coffeeSacksRegister = new RegisterCoffeeSacks();
+
+            coffeeSacksRegister.initialize(config);
+        }
+    }
+}
---
+++ SaxonHE11NetIXQuery/coffeesacks-in-xquery-example1.xq
@@ -0,0 +1,15 @@
+﻿declare namespace cs = "http://nineml.com/ns/coffeesacks";
+
+declare namespace output = "http://www.w3.org/2010/xslt-xquery-serialization";
+
+declare option output:method 'xml';
+declare option output:indent 'yes';
+
+declare variable $input-date as xs:string external := '1 September 2022';
+
+<invisible-xml-test>
+{
+let $grammar := cs:grammar-uri('date.ixml')
+return cs:parse-string($grammar, $input-date)
+}
+</invisible-xml-test>
